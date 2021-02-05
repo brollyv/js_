e89c6bc6614e137bdc1d6ce6762c6c7d5e8612d4
Alexandru Vieriu <alexandru_vieriu@smart-x.net>
2021-02-05 15:04:25 +0200

    more functions

---
+++ complete_course/02-Fundamentals-Part-2/starter/reviewingFunctions.js
@@ -0,0 +1,29 @@
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+
+    if (retirement > 0) {
+        // after return the function immediatelly exits, no longer continues. 
+        console.log(`${firstName} retires in ${retirement} years`);
+        return retirement;
+    } else {
+        console.log(`${firstName} already retired`);
+        return -1;
+    }
+
+    //return `${firstName} retires in ${retirement} years`;
+
+}
+
+
+
+
+
+console.log(yearsUntilRetirement(1991, 'Alex'));
+
+console.log(yearsUntilRetirement(1950, 'Mike'));
