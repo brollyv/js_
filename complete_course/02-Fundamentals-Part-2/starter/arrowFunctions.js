faba6c0e5ac6d0297903548692584889098a0795
Alexandru Vieriu <alexandru_vieriu@smart-x.net>
2021-02-05 14:33:56 +0200

    arrow functions

--- complete_course/02-Fundamentals-Part-2/starter/arrowFunctions.js
+++ complete_course/02-Fundamentals-Part-2/starter/arrowFunctions.js
@@ -1,1 +1,20 @@
 // Arrow functions
+
+// return happens implicitly only for one liner functions
+
+const calcAge3 = birthYear => 2037 - birthYear;
+
+const age3 = calcAge3(1988);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    //return retirement;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1988, 'Alex'));
+console.log(yearsUntilRetirement(1980, 'Bob'));
+
+// Arrow functions do not get the "this" keyword
