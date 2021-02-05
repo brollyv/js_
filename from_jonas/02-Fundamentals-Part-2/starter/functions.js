f064ee4f776cee74ce02fecca38aeeb2fe1c80a4
Alexandru Vieriu <alexandru_vieriu@smart-x.net>
2021-02-05 10:09:39 +0200

    js_

--- from_jonas/02-Fundamentals-Part-2/starter/functions.js
+++ from_jonas/02-Fundamentals-Part-2/starter/functions.js
@@ -1,3 +1,25 @@
 // Functions
 
-//test
+function logger() {
+    console.log('This is a Logger function');
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(3, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(3, 4));
+
+const appleOrangeJuice = fruitProcessor(2, 5);
+console.log(appleOrangeJuice);
+
+// not all functions need to return something or have params
+
