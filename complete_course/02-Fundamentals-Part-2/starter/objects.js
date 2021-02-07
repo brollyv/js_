4f7c7ea5aaf2c7f912fb981b3a16ed7c4dd7fdb9
Alexandru Vieriu <alexandru_vieriu@smart-x.net>
2021-02-07 14:26:41 +0200

    Objects

---
+++ complete_course/02-Fundamentals-Part-2/starter/objects.js
@@ -0,0 +1,39 @@
+// Objects
+
+const alex = {
+    firstName: 'Alex',
+    lastName: 'Vieriu',
+    age: 2037 - 1988,
+    job: 'programmer',
+    friends: ['Michael', 'Peter', 'Steven']
+};
+
+console.log(alex);
+
+// Dot notation
+
+console.log(alex.lastName);
+
+// Brackets notation
+
+console.log(alex['firstName']);
+
+const nameKey = 'Name';
+console.log(alex['first' + nameKey]);
+console.log(alex['last' + nameKey]);
+
+const interestedIn = prompt('What do you want to know about me? Choose between firstName, lastName, age, job and friends');
+
+
+if (alex[interestedIn]) {
+    console.log(alex[interestedIn]);
+
+} else {
+    console.log('Wrong request ... Choose between firstName, lastName, age, job and friends');
+}
+
+alex.location = 'Barcelona';
+alex['twitter'] = '@developful';
+console.log(alex);
+
+console.log(`${alex.firstName} has ${alex.friends.length} friends, and his best friend is ${alex.friends[0]}`);
