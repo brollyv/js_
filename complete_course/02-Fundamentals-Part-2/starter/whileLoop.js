09ee6daab60be9599c46e2f9778a9e35a6010316
Alexandru Vieriu <alexandru_vieriu@smart-x.net>
2021-02-08 23:59:26 +0200

    while and coding challenge

---
+++ complete_course/02-Fundamentals-Part-2/starter/whileLoop.js
@@ -0,0 +1,22 @@
+// While Loop
+
+// for (let rep = 1; rep <= 10; rep++) {
+//     console.log(`Lifting weights repetition ${rep}`);
+// }
+
+// for while Loop we can only specify a condition
+
+let rep = 1;
+while (rep <= 10) {
+    //   console.log(`While Loop ---> Lifting weights repetition ${rep}`);
+    rep++;
+}
+
+let dice = Math.trunc((Math.random() * 6) + 1);
+console.log(dice);
+
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc((Math.random() * 6) + 1);
+    if (dice === 6) console.log('Loop is about to end ...');
+}
