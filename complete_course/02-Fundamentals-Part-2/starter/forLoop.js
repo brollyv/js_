529bb1bdffebf97553415ade18a9f13b949d5599
Alexandru Vieriu <alexandru_vieriu@smart-x.net>
2021-02-08 22:14:55 +0200

    for loop example

---
+++ complete_course/02-Fundamentals-Part-2/starter/forLoop.js
@@ -0,0 +1,7 @@
+// Iteration - The foor Loop
+
+
+// for loop keeps running while condition is TRUE
+for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifting wights repetition ${rep} 🏋️‍♀️`);
+}
