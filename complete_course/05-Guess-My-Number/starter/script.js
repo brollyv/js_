46694250ed0263ba34bbd9eb93a3df11e2d4e9a4
Alexandru Vieriu <alexandru_vieriu@smart-x.net>
2021-02-10 01:26:39 +0200

    dom and events

--- complete_course/05-Guess-My-Number/starter/script.js
+++ complete_course/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+// manipulating elements
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.number').textContent = 13;
+
+// document.querySelector('.score').textContent = 21;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+// handling click events
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No number';
+  }
+});
