730ef67328c6b0e15c8ec2a73d46471f1aa1ddeb
Alexandru Vieriu <alexandru_vieriu@smart-x.net>
2021-02-12 02:19:39 +0200

    modal

--- complete_course/06-Modal/starter/script.js
+++ complete_course/06-Modal/starter/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// we're not writing closemodal() because we don't want the function to be invoked when js reads the line but only the event happens.
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
