45dcae2ce32e4b820226bc41f0bd7487c5c224c0
Alexandru Vieriu <alexandru_vieriu@smart-x.net>
2021-02-17 11:19:37 +0200

    s

--- complete_course/09-Data-Structures-Operators/starter/playing.js
+++ complete_course/09-Data-Structures-Operators/starter/playing.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const alex = {
+  firstName: 'Mihai',
+  middleName: 'Alexandru',
+  lastName: 'Vieriu',
+  dateOfBirth: [12, 12, 1988],
+  hobbies: ['coding', 'sports', 'cooking', 'movies'],
+  streetNames: ['Mihai Bravu', 'Miorita'],
+  streetNumbers: ['85-93', '83N'],
+  favoriteFood: 'Pizza',
+
+  age: function (year) {
+    console.log(year - this.dateOfBirth[2]);
+  },
+};
+
+let [birthDay, birthMonth, birthYear] = alex.dateOfBirth;
+
+let x = ([birthDay, birthMonth, birthYear] = [birthYear, birthMonth, birthDay]);
+console.log(x);
+
+console.log(birthDay, birthMonth, birthYear);
+
+alex.age(2021);
+
+const newHobbies = [alex.hobbies, ...x];
+
+console.log(newHobbies);
