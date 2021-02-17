02722603d83fb99a4e990fbd42462a5473e129f4
Alexandru Vieriu <alexandru_vieriu@smart-x.net>
2021-02-17 10:56:20 +0200

    rest pattern and params

---
+++ complete_course/09-Data-Structures-Operators/starter/restPatternAndParameters.js
@@ -0,0 +1,98 @@
+'use strict';
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order received!  ${this.starterMenu[starterIndex]} and  ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}.`
+    );
+  },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
+
+// 1) Destructuring
+// Rest pattern is used to pack elements into an Array
+
+// SPERAD because on the RIGHT side of =
+
+const arr = [1, 2, 3, ...[3, 4]];
+
+// REST because on the left side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5]; // takes the remaining elements and put them into a new array called others. see console log
+
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+
+console.log(pizza, risotto, otherFood);
+
+// Objects
+
+const { sat, ...weekdays } = restaurant.openingHours;
+
+console.log(weekdays, sat);
+
+// 2) Functions (rest parameters)
+
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 4, 3, 1);
+
+const x = [23, 5, 7];
+
+add(...x);
+
+restaurant.orderPizza(
+  'peperoncino',
+  'blue cheese',
+  'olives',
+  'salami',
+  'spinach'
+);
